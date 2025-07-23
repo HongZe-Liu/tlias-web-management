b4540563463cc3ec0770711a6e0c50ba5f0470e4
Hongze-Liu <hongzeliu2013@gmail.com>
2025-07-23 20:32:32 +0200

    初始化项目 - 添加部门管理功能
    - 创建部门实体类Dept
    - 添加部门控制器DeptController
    - 添加部门服务层DeptService
    - 配置MyBatis和数据库连接

---
+++ src/main/java/com/itheima/controller/DeptController.java
@@ -0,0 +1,10 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 部门管理控制器
+ */
+@RestController
+public class DeptController {
+}
---
+++ src/main/java/com/itheima/service/Impl/DeptServiceImpl.java
@@ -0,0 +1,8 @@
+package com.itheima.service.Impl;
+
+import com.itheima.service.DeptService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+}
