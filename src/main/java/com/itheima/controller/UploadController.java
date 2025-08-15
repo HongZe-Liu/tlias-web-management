644463b9918825883cf6ea936cba2d180ca90d7b
Hongze-Liu <hongzeliu2013@gmail.com>
2025-08-15 21:09:34 +0200

    update

--- src/main/java/com/itheima/controller/UploadController.java
+++ src/main/java/com/itheima/controller/UploadController.java
@@ -1,4 +1,21 @@
 package com.itheima.controller;
 
+import com.itheima.pojo.Result;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController("")
 public class UploadController {
+
+    private static final Logger log = LoggerFactory.getLogger(UploadController.class);
+    @PostMapping("/upload")
+    public Result upload(String name, Integer age, MultipartFile file) {
+        log.info("接收到的参数：{},{}.{}", name,age,file);
+        return Result.success();
+    }
 }
--- src/main/java/com/itheima/service/Impl/EmpServiceImpl.java
+++ src/main/java/com/itheima/service/Impl/EmpServiceImpl.java
@@ -3,14 +3,14 @@ package com.itheima.service.Impl;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.itheima.mapper.EmpExprMapper;
+import com.itheima.mapper.EmpLogMapper;
 import com.itheima.mapper.EmpMapper;
-import com.itheima.pojo.Emp;
-import com.itheima.pojo.EmpExpr;
-import com.itheima.pojo.EmpQueryParam;
-import com.itheima.pojo.PageResult;
+import com.itheima.pojo.*;
+import com.itheima.service.EmpLogService;
 import com.itheima.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDateTime;
@@ -25,7 +25,14 @@ public class EmpServiceImpl implements EmpService {
 
     @Autowired
     private EmpMapper empMapper;
+
+    @Autowired
     private EmpExprMapper empExprMapper;
+
+    @Autowired
+    private EmpLogMapper empLogMapper;
+    @Autowired
+    private EmpLogService empLogService;
 
     @Override
     public PageResult page(EmpQueryParam empQueryParam) {
@@ -43,24 +50,32 @@ public class EmpServiceImpl implements EmpService {
     /**
      * 新增员工
      */
-
+    @Transactional(rollbackFor = {Exception.class} ) // 事务管理注解
     @Override
     public void save(Emp emp) {
-        // 1.保存员工基本信息
-        emp.setCreateTime(LocalDateTime.now());
-        emp.setUpdateTime(LocalDateTime.now());
-        empMapper.insert(emp);
+        try {
+            // 1.保存员工基本信息
+            emp.setCreateTime(LocalDateTime.now());
+            emp.setUpdateTime(LocalDateTime.now());
+            empMapper.insert(emp);
 
-        // 2. 保存员工工作经历
-        List<EmpExpr> exprList = emp.getEmpList();
+            // 2. 保存员工工作经历
+            List<EmpExpr> exprList = emp.getEmpList();
             // 判断是否为空
-             if(!CollectionUtils.isEmpty(exprList)){
-                 // 遍历集合，为empId赋值
-                 exprList.forEach(empExpr -> {
-                     empExpr.setEmpId(emp.getId());
-                 });
-                 // Batch后缀表示批量保存
+            if (!CollectionUtils.isEmpty(exprList)) {
+                // 遍历集合，为empId赋值
+                exprList.forEach(empExpr -> {
+                    empExpr.setEmpId(emp.getId());
+                });
+                // Batch后缀表示批量保存
                 empExprMapper.insetBatch(exprList);
-             }
+            }
+        } finally {
+            // 记录操作日志
+            EmpLog empLog = new EmpLog(null,LocalDateTime.now(),emp.toString());
+                // 创建日志对象用于保存操作日志
+            empLogService.insertLog(empLog);
+                // 调用方法将日志插入到对象中
+        }
     }
 }
